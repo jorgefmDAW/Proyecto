3dd9e6b473f89cfeae58688e2d68dd368c8d9620
Nacho Yelo <nachoyelo.belda@gmail.com>
2026-06-21 22:30:58 +0200

    Añadidas páginas públicas: cómo funciona y reglas

---
+++ frontend/src/app/pages/reglas-puntuacion/reglas-puntuacion.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-reglas-puntuacion',
+  imports: [RouterLink],
+  templateUrl: './reglas-puntuacion.html',
+  styleUrl: './reglas-puntuacion.css',
+})
+export class ReglasPuntuacion {
+
+}
